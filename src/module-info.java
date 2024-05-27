9320a38fc10df56942075ce15cb692ec874446af
Niks <167508665+lomonosovs@users.noreply.github.com>
2024-05-27 22:21:54 +0300

    darba sākums

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module Lomonosovs_pica {
+}
---
+++ src/niksLomonosovs/Lomonosovs_pica.java
@@ -0,0 +1,7 @@
+package niksLomonosovs;
+
+public class Lomonosovs_pica {
+	public static void main(String[] args) {
+		
+	}
+}
