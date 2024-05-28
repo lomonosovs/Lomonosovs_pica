e08c772732710df9e2a0cb849dbf8fb8426c4390
Niks <niks.lomonosovs91@gmail.com>
2024-05-28 20:10:24 +0300

    add fields and constructor to Order class

--- src/niksLomonosovs/Order.java
+++ src/niksLomonosovs/Order.java
@@ -1,5 +1,32 @@
 package niksLomonosovs;
 
 public class Order {
+    private String vards; 
+    private int tel; 
+    private boolean pegade; 
+    private String adrese; 
 
+    public Order(String vards, int tel, boolean pegade) { 
+        this.vards = vards; 
+        this.tel = tel; 
+        this.pegade = pegade; 
+    } 
+
+    public void setAddress(String adrese) { 
+        this.adrese = adrese; 
+    } 
+
+    public double getTotalCost() { 
+        double totalCost = 0; 
+        
+        return totalCost; 
+    } 
+
+    public String toString() {
+        String rez = "\nvards: " + vards + "\nTelefona numurs: " 
+                + tel + "\nPiegade: " + (pegade ? "Ja +5$" : "Ne") +
+                "\nAdrese: " + adrese + "\nPizzas:"; 
+        rez += "\nTotal Cost: $" + getTotalCost(); 
+        return rez; 
+    } 
 }
